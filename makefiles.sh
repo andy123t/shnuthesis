d40994e93e72d91f6df21361ba2f531dd0cf9965
andy123t <andy123t@163.com>
2023-04-10 22:38:44 +0800

    update

--- makefiles.sh
+++ makefiles.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#-> Get source file
-find . -maxdepth 1 -type f -name "*.tex" | while read file; do 
+#-> Get source FileName
+find . -maxdepth 1 -type f -name "*.tex" | while read FileName; do 
 #--- Process file.tex ---
-echo "$file"
-echo "${file%.tex}"
+echo "$FileName"
+echo "${FileName%.tex}"
 #-> Compile the main file
-xelatex --synctex=-1 "$file"
-bibtex "${file%.tex}"
-xelatex --synctex=-1 "$file"
-xelatex --synctex=-1 "$file"
+xelatex --synctex=-1 "$FileName"
+bibtex "${FileName%.tex}"
+xelatex --synctex=-1 "$FileName"
+xelatex --synctex=-1 "$FileName"
 #-> Clear aux files
 rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb 
 done
